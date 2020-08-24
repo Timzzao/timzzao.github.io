d6d5de2809a5ce82a2fc6e1d99d10500737c1a33
timzzao <vinicius.nsilva30@gmail.com>
2020-08-24 20:21:15 -0300

    Updates

---
+++ precache-manifest.fadfbb2631269fba0614a6c782085acf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a266c8fe12f8179a6e11a8fafa3f94e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68d666860f5d5b3c2be4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98ba9b22910f71c9bb02",
+    "url": "/static/js/2.82434f93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.82434f93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d666860f5d5b3c2be4",
+    "url": "/static/js/main.c06f36ba.chunk.js"
+  },
+  {
+    "revision": "53e0d7af479f7a23c1ee",
+    "url": "/static/js/runtime-main.4ba972ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
