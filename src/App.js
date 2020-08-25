b2fafc9c6e3765618d11b1fea81be0a7dbe9a8ea
timzzao <vinicius.nsilva30@gmail.com>
2020-08-25 11:39:34 -0300

    Initial changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav } from 'react-bootstrap'
+import { Row, Col, Image } from 'react-bootstrap'
+import profileImage from './images/profile.jpg'
+import { Route } from "react-router";
+import Home from './Home/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand >
+          <Row>
+            <Col xs={2} md={1}>
+              <Image src={profileImage} roundedCircle width={30} height={30}/>
+            </Col>
+          </Row>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link componemt={Home}>Home</Nav.Link>
+            <Nav.Link href="#link">Portifolio</Nav.Link>
+            <Nav.Link href="#link">About Me</Nav.Link>
+            <Nav.Link href="#link">Contact Me</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
